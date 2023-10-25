bb0118ad58a5e7b5c36fec146deaf62eed94e3b0
arim83 <jorammercado@pursuit.org>
2023-10-25 00:57:18 -0400

    seed read done

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Thunderstruck', 'AC/DC', 'The Razors Edge', '4:53', false),
+('One', 'Creed','My Own Prison','5:01', true),
+('Take it to the Limit', 'Eagles', 'One of These Nights','5:14', true),
+('Highway to Helll', 'AC/DC', 'Highway to Hell', '3:28', false),
+('My Sacrifice', 'Creed','Weathered','4:40', true),
+('Hotel California', 'Eagles', 'Hotel California','6:30', true);
---
+++ queries/song.js
@@ -0,0 +1,13 @@
+const db = require("../db/dbConfig.js")
+
+const getAllSongs = async () => {
+    try{
+        const allSongs = await db.any("SELECT * FROM songs")
+        return allSongs
+    }
+    catch (error) {
+        return error
+    }
+}
+
+module.exports = { getAllSongs }
